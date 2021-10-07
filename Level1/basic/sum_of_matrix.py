72e4bc8e952c6e4c934cca97df65a63b91249246
moon-jueun <juliy9812@publicai.co.kr>
2021-10-08 00:06:00 +0900

    행렬의 덧셈

---
+++ Level1/basic/sum_of_matrix.py
@@ -0,0 +1,13 @@
+# 행렬의 덧셈
+def solution(arr1, arr2):
+    arr1 = [[1,2],[2,3]]
+    arr2 = [[3,4],[5,6]]
+    answer = []
+
+
+    for i in range(len(arr1)):
+        answer_1 = []
+        for j in range(len(arr1[i])):
+            answer_1.append(arr1[i][j] + arr2[i][j])
+        answer.append(answer_1)
+    return answer
